05b3753b975b5a0575f2fcb55b1e0ad11c69d00e
DariusGracey <Dariusgracey814@gmail.com>
2023-03-20 23:20:32 -0400

    create delete rest api methods for db delete query

--- application/src/main/java/com/budgetly/application/springMVC/controller/BudgetController.java
+++ application/src/main/java/com/budgetly/application/springMVC/controller/BudgetController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -79,6 +80,12 @@ public class BudgetController {
 		ResponseEntity.created(location).build();
 		
 		return "createBudget.jsp";
+	}
+	
+	// Delete budget
+	@DeleteMapping(path = "/user/{id}/budgets/delete-budget/{budgetId}")
+	public void deleteBudget(@PathVariable int budgetId) {
+		service.deleteBudget(budgetId);
 	}
 	
 }
--- application/src/main/java/com/budgetly/application/springMVC/controller/ExpenseController.java
+++ application/src/main/java/com/budgetly/application/springMVC/controller/ExpenseController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,7 +59,7 @@ public class ExpenseController {
 	}
 	
 	// Save expense to Db
-	@PostMapping(path = "/budget/{budgetId}/expenses") 
+	@PostMapping(path = "/user/{id}/budgets/{budgetId}/expenses") 
 	public String addExpense(@RequestBody Expense expense) {
 		Expense savedExpense = service.saveExpense(expense);
 		
@@ -79,5 +80,11 @@ public class ExpenseController {
 		return "createExpense.jsp";
 	}
 	
+	
+	// Delete budget
+	@DeleteMapping(path = "/users/{id}/budgets/{budgetId}/expenses/{expId}")
+	public void deleteExpense(@PathVariable int expId) {
+		service.deleteExpense(expId);
+	}
 	
 }
